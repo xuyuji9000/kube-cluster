fdc36ef7ead8355f44123936299b9af8470126ae
Karl Xu <karl.yogi.xu@gmail.com>
2018-09-30 00:57:10 +0800

    remove log from master user-data

--- master/master-user-data.sh
+++ master/master-user-data.sh
@@ -8,8 +8,6 @@ KUBECTL_HOME=/root
 
 cd $KUBECTL_HOME
 
-exec &> logfile.txt
-
 # install Docker
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
