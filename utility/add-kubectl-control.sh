b2da5c9ed95fd6343db505a0cdc0224d6272bf9e
Karl Xu <karl.yogi.xu@gmail.com>
2018-10-03 01:18:28 +0800

    add parameter checking to utility/add-kubectl-control.sh

--- utility/add-kubectl-control.sh
+++ utility/add-kubectl-control.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # $1 is kubernetes cluster master public ip
+if [ -z $1 ]
+then
+    echo "Does not add parameter."; exit 1;
+fi
 
 KUBE_CONFIG="${HOME}/.kube/config"
 REMOTE_USER_PATH="/root"
