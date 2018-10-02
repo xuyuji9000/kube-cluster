2926b14f5d8c846e884cdae1a689808fd9137e48
Karl Xu <karl.yogi.xu@gmail.com>
2018-10-03 00:11:13 +0800

    add join command to join-workers

--- utility/join-workers.sh
+++ utility/join-workers.sh
@@ -7,4 +7,9 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 if [ $1 = test ]
 then
     ansible all -m ping -i hosts
+elif [ $1 = join ]
+then
+    ansible-playbook -i hosts workers.yml
+else
+    echo 'Does not recognize the command.'
 fi
