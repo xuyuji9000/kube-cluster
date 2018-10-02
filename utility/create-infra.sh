4f6f210318391f1639208dd44370dd4c3f5d8114
Karl Xu <karl.yogi.xu@gmail.com>
2018-10-02 18:07:58 +0800

    add destroy command to scripts

--- utility/create-infra.sh
+++ utility/create-infra.sh
@@ -2,8 +2,16 @@
 
 # Get digitalocean token
 export TF_VAR_do_token=$(cat ~/.config/doctl/config.yaml | grep token | awk '{print $2}')
-echo  $(cat ~/.config/doctl/config.yaml | grep token | awk '{print $2}')
+# echo  $(cat ~/.config/doctl/config.yaml | grep token | awk '{print $2}')
 
 
+if [ $1 = apply ]
+then
 # provions servers
-terraform apply
+    terraform apply
+elif [ $1 = destroy ]
+then
+    terraform destroy
+else
+    echo 'Does not recognize the command.'
+fi
