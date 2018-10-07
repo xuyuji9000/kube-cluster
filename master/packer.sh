e5c5af3a5ac618645e4d181ff6c50e9f9e3b0b12
Karl Xu <karl.yogi.xu@gmail.com>
2018-10-07 10:17:08 +0800

    encapsulate master packer.json

---
+++ master/packer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DOCKER_VERSION=17.03
+KUBE_VERSION=1.11.3-00
+KUBE_CNI_VERSION=0.6.0-00
+
+# install Docker
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep ${DOCKER_VERSION} | head -1 | awk '{print $3}')
+
+
+# Install Kubernentes dependency
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update && apt-get install -y kubernetes-cni=${KUBE_CNI_VERSION} kubelet=${KUBE_VERSION} kubeadm=${KUBE_VERSION} kubectl=${KUBE_VERSION}
+apt-mark hold kubelet kubeadm kubectl
